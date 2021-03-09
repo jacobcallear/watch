20b26b1835f8a964ba0f8f3e65d8eb1ce16a97f4
Jacob Callear <jacob18@callear.net>
2021-03-09 11:06:54 +0000

    Execute command from command line input

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
+use std::env;
 use std::process::Command;
 use std::process::Output;
 
 fn main() {
-    let output = execute_shell_command("git", &vec!["status"]);
+    // Parse command line args
+    let mut args = env::args().skip(1);
+    let command = args.next().unwrap();
+    let args: Vec<String> = args.collect();
 
+    // Execute command and print stdout
+    let output = execute_shell_command(command.as_str(), &args);
     println!("{}", String::from_utf8_lossy(&output.stdout));
 }
 
-fn execute_shell_command(command: &str, args: &Vec<&str>) -> Output {
+/// Executes a shell command and returns output
+fn execute_shell_command(command: &str, args: &Vec<String>) -> Output {
     let args = args.to_vec();
 
     Command::new(command)
