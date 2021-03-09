8259d7ce8173b2cff87b90bf3b3a241c795efec8
Jacob Callear <jacob18@callear.net>
2021-03-09 11:35:29 +0000

    Allow user to set pause time between commands

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,29 @@ use std::time::Duration;
 use std::thread;
 
 fn main() {
-    // Parse command line args
     let mut args = env::args().skip(1);
-    let command = args.next().unwrap();
+
+    // Get pause time if -i flag given, otherwise default to 2 seconds
+    // Get passed shell command
+    let first_arg = args.next().expect("expected a shell command");
+    let (pause_seconds, command) = if first_arg == String::from("-i") {
+        let error_message = "expected a whole number of seconds after `-i` flag";
+        let pause_seconds: u64 = args.next()
+            .expect(error_message)
+            .parse().expect(error_message);
+        (pause_seconds, args.next().unwrap())
+    } else {
+        (2, first_arg)
+    };
+
     let args: Vec<String> = args.collect();
 
-    // Execute command and print stdout
+    // Execute command and print stdout and stderr
     loop {
         let output = execute_shell_command(command.as_str(), &args);
         println!("{}", String::from_utf8_lossy(&output.stderr));
         println!("{}", String::from_utf8_lossy(&output.stdout));
-        thread::sleep(Duration::from_secs(2));
+        thread::sleep(Duration::from_secs(pause_seconds));
     }
 }
 
