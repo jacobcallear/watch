f7d9298aad92316a8959579d6ca987a955ff6ab7
Jacob Callear <jacob18@callear.net>
2021-03-09 12:07:40 +0000

    Split code into separate files

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use std::env;
+use std::process::Command;
+use std::process::Output;
+
+pub struct Config {
+    pub pause_seconds: u64,
+    pub command: String,
+    pub args: Vec<String>,
+}
+
+impl Config {
+    pub fn new(mut args: env::Args)-> Result<Config, &'static str> {
+        args.next();
+        let first_arg = match args.next() {
+            Some(arg) => arg,
+            None => return Err("didn't get a shell command"),
+        };
+
+        let (pause_seconds, command) = if first_arg == String::from("-i") {
+            let error_message = "didn't get a whole number of seconds after `-i` flag";
+            let pause_seconds: u64 = match args.next() {
+                Some(arg) => {
+                    match arg.parse() {
+                        Ok(int) => int,
+                        _ => return Err(error_message),
+                    }
+                },
+                None => return Err(error_message),
+            };
+            let command = match args.next() {
+                Some(command) => command,
+                None => return Err("didn't get a shell command"),
+            };
+
+            (pause_seconds, command)
+        } else {
+            (2, first_arg)
+        };
+
+        let args: Vec<String> = args.collect();
+
+        Ok(Config {
+            pause_seconds,
+            command,
+            args,
+        })
+    }
+}
+
+/// Executes a shell command and returns output
+pub fn execute_shell_command(command: &str, args: &Vec<String>) -> Output {
+    let args = args.to_vec();
+
+    Command::new(command)
+        .args(&args)
+        .output()
+        .expect("could not execute command")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,10 @@
 use std::env;
 use std::process;
-use std::process::Command;
-use std::process::Output;
 use std::time::Duration;
 use std::thread;
 
-struct Config {
-    pause_seconds: u64,
-    command: String,
-    args: Vec<String>,
-}
-
-impl Config {
-    fn new(mut args: env::Args)-> Result<Config, &'static str> {
-        args.next();
-        let first_arg = match args.next() {
-            Some(arg) => arg,
-            None => return Err("didn't get a shell command"),
-        };
-
-        let (pause_seconds, command) = if first_arg == String::from("-i") {
-            let error_message = "didn't get a whole number of seconds after `-i` flag";
-            let pause_seconds: u64 = match args.next() {
-                Some(arg) => {
-                    match arg.parse() {
-                        Ok(int) => int,
-                        _ => return Err(error_message),
-                    }
-                },
-                None => return Err(error_message),
-            };
-            let command = match args.next() {
-                Some(command) => command,
-                None => return Err("didn't get a shell command"),
-            };
-
-            (pause_seconds, command)
-        } else {
-            (2, first_arg)
-        };
-
-        let args: Vec<String> = args.collect();
-
-        Ok(Config {
-            pause_seconds,
-            command,
-            args,
-        })
-    }
-}
-
+use watch::execute_shell_command;
+use watch::Config;
 
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
@@ -64,14 +19,4 @@ fn main() {
         println!("{}", String::from_utf8_lossy(&output.stdout));
         thread::sleep(Duration::from_secs(config.pause_seconds));
     }
-}
-
-/// Executes a shell command and returns output
-fn execute_shell_command(command: &str, args: &Vec<String>) -> Output {
-    let args = args.to_vec();
-
-    Command::new(command)
-        .args(&args)
-        .output()
-        .expect("could not execute command")
 }
